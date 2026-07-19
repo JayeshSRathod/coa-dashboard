30d03785369fd40f1a7a3bfb938b6d490602dcf4
Jayesh Rathod <152681955+JayeshSRathod@users.noreply.github.com>
2026-07-19 16:09:22 +0530

    Sprint-009: remove analytics import cycle

--- src/analytics/__init__.py
+++ src/analytics/__init__.py
@@ -2,6 +2,5 @@
 
 from .engine import PerformanceAnalyticsEngine
 from .models import AnalyticsReport, CompletedTrade, PerformanceSnapshot
-from .service import AnalyticsService
 
-__all__ = ["AnalyticsReport", "AnalyticsService", "CompletedTrade", "PerformanceAnalyticsEngine", "PerformanceSnapshot"]
+__all__ = ["AnalyticsReport", "CompletedTrade", "PerformanceAnalyticsEngine", "PerformanceSnapshot"]
